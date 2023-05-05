790b61fd59bd314c86ea1076c2e01dd2d2b55b97
Georgi Martsenkov <g.martsenkov@gmail.com>
2023-05-05 14:15:01 +0100

    Serial IO

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,14 @@ use embedded_hal::{digital::v2::OutputPin, timer::CountDown};
 use fugit::ExtU32;
 use panic_halt as _;
 use rp_pico::{entry, hal};
+use serde::{Deserialize, Serialize};
 use usb_device::{class_prelude::UsbBusAllocator, prelude::*};
-use usbd_human_interface_device::{device::joystick::JoystickReport, prelude::*};
+use usbd_serial::{SerialPort, USB_CLASS_CDC};
+
+#[derive(Deserialize, Serialize)]
+struct Config {
+    load_cell_kg: bool,
+}
 
 #[entry]
 fn main() -> ! {
@@ -41,38 +47,84 @@ fn main() -> ! {
         &mut pac.RESETS,
     ));
 
-    let mut joy = UsbHidClassBuilder::new()
-        .add_device(usbd_human_interface_device::device::joystick::JoystickConfig::default())
-        .build(&usb_bus);
+    let mut serial = SerialPort::new(&usb_bus);
 
     let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x1209, 0x0001))
         .manufacturer("gogo")
         .product("cool pedals")
         .serial_number("G-CP000X")
+        .device_class(USB_CLASS_CDC)
         .build();
 
     let mut input_count_down = timer.count_down();
-    input_count_down.start(10.millis());
+    input_count_down.start(1000.millis());
 
     let mut led_pin = pins.led.into_push_pull_output();
     led_pin.set_high().unwrap();
 
+    let mut buffer = [0; 64];
+    let mut write_buffer = [0; 64];
+
     loop {
         if input_count_down.wait().is_ok() {
-            let report = JoystickReport {
-                buttons: 0,
-                x: 1,
-                y: 1,
-            };
-            match joy.device().write_report(&report) {
-                Err(UsbHidError::WouldBlock) => {}
-                Ok(_) => {}
-                Err(_) => {
-                    core::panic!("Failed to write joystic report: {:?}", report)
+            match serial.read(&mut buffer[..]) {
+                Ok(_) => {
+                    serial.write(&buffer).ok();
+                    match serde_json_core::de::from_slice::<Config>(&buffer) {
+                        Ok(config) => {
+                            serde_json_core::ser::to_slice(&config, &mut write_buffer).unwrap();
+                            serial.write(&write_buffer).ok()
+                        }
+                        Err(e) => serial.write(json_error_to_str(e).as_bytes()).ok(),
+                    }
                 }
-            }
+                Err(_) => Some(0),
+            };
         }
 
-        if usb_dev.poll(&mut [&mut joy]) {}
+        if usb_dev.poll(&mut [&mut serial]) {}
+    }
+}
+
+fn json_error_to_str(e: serde_json_core::de::Error) -> &'static str {
+    match e {
+        serde_json_core::de::Error::EofWhileParsingList => "EOF while parsing a list.",
+        serde_json_core::de::Error::EofWhileParsingObject => "EOF while parsing an object.",
+        serde_json_core::de::Error::EofWhileParsingString => "EOF while parsing a string.",
+        serde_json_core::de::Error::EofWhileParsingValue => "EOF while parsing a JSON value.",
+        serde_json_core::de::Error::ExpectedColon => "Expected this character to be a `':'`.",
+        serde_json_core::de::Error::ExpectedListCommaOrEnd => {
+            "Expected this character to be either a `','` or\
+                     a \
+                     `']'`."
+        }
+        serde_json_core::de::Error::ExpectedObjectCommaOrEnd => {
+            "Expected this character to be either a `','` \
+                     or a \
+                     `'}'`."
+        }
+        serde_json_core::de::Error::ExpectedSomeIdent => {
+            "Expected to parse either a `true`, `false`, or a \
+                     `null`."
+        }
+        serde_json_core::de::Error::ExpectedSomeValue => {
+            "Expected this character to start a JSON value."
+        }
+        serde_json_core::de::Error::InvalidNumber => "Invalid number.",
+        serde_json_core::de::Error::InvalidType => "Invalid type",
+        serde_json_core::de::Error::InvalidUnicodeCodePoint => "Invalid unicode code point.",
+        serde_json_core::de::Error::KeyMustBeAString => "Object key is not a string.",
+        serde_json_core::de::Error::TrailingCharacters => {
+            "JSON has non-whitespace trailing characters after \
+                     the \
+                     value."
+        }
+        serde_json_core::de::Error::TrailingComma => {
+            "JSON has a comma after the last value in an array or map."
+        }
+        serde_json_core::de::Error::CustomError => {
+            "JSON does not match deserializer’s expected format."
+        }
+        _ => unimplemented!()
     }
 }
